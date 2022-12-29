e2b7e8e16ae796854f7fec495075d231bb357a90
altanberkeren <101469988+altanberkeren@users.noreply.github.com>
2022-12-29 17:06:30 +0000

    - Log %95 done

--- Log.h
+++ Log.h
@@ -36,8 +36,8 @@ void logCoffee(int coffeeCups[], int coinsAmmount[],std::string coffeeNames[]){
         switch (i){
         
         case 0:
-                dbW<<"0.5: "<<coinsAmmount[i]<<'\n';
-                total += 0.5*coinsAmmount[i];
+                dbW<<"0.05: "<<coinsAmmount[i]<<'\n';
+                total += 0.05*coinsAmmount[i];
             break;
 
         case 1:
@@ -51,21 +51,21 @@ void logCoffee(int coffeeCups[], int coinsAmmount[],std::string coffeeNames[]){
             break;
         
         case 3:
-                dbW<<"1: "<<coinsAmmount[i]<<'\n';
-                total += 1*coinsAmmount[i];
+                dbW<<"0.5: "<<coinsAmmount[i]<<'\n';
+                total += 0.5*coinsAmmount[i];
             break;
 
         case 4:
-                dbW<<"5: "<<coinsAmmount[i]<<'\n';
-                total += 5*coinsAmmount[i];
+                dbW<<"1: "<<coinsAmmount[i]<<'\n';
+                total += 1*coinsAmmount[i];
             break;
         case 5:
-                dbW<<"10: "<<coinsAmmount[i]<<'\n';
+                dbW<<"2: "<<coinsAmmount[i]<<'\n';
                 total += 10*coinsAmmount[i];
             break;
         case 6:
-                dbW<<"20: "<<coinsAmmount[i]<<'\n';
-                total += 20*coinsAmmount[i];
+                dbW<<"5: "<<coinsAmmount[i]<<'\n';
+                total += 5*coinsAmmount[i];
             break;
         default:
             break;
